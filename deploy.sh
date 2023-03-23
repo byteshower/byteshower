85cfea380a129508b889e30faa55147e2dc1358e
Munir Hossain <mhossain@infoblox.com>
2023-03-23 15:05:30 -0700

    fixing baseurl and excluding CNAME file to be overwritten.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "* checking out the master branch:"
 git clone --single-branch --branch master git@github.com:byteshower/byteshower.github.io.git master
 
 echo "* synchronizing the files:"
-rsync -arv public/ master --delete --exclude ".git"
+rsync -arv public/ master --delete --exclude ".git" --exclude "CNAME"
 cp README.md master/
 
 echo "* pushing to master:"
